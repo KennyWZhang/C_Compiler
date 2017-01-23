ec5476ee81a5daa687c46dc44795ef12eb0f522c
Kenny.W.Zhang <victorypiter@msn.com>
2017-01-23 21:24:15 +0800

    update compiler1.c

--- Lesson_1/compiler0.c
+++ Lesson_1/compiler0.c
@@ -2,50 +2,67 @@
 #include <stdlib.h>
 #include <memory.h>
 
-int poolsize;         // default size of text/stack
-int *text,            // text segment
-    *stack;           // stack, moves from hight address to low address
-int *pc, *sp, ax; // virtual machine registers
+int poolsize;         // the size of text and stack area
+
+int *text,            // text area
+    *stack;           // the stack, grows from hight address to low address
+
+int *pc, // the program counter pointer
+    *sp, // the stack (top) pointer
+    ax; // the ax register
 
 // instructions
 enum {
     LEA, IMM, JMP, CALL, JZ, JNZ, ENT, ADJ, LEV, LI, LC, SI ,SC, PUSH,
-    OR, XOR, AND, EQ, NE, LT, GT, LE, GE, SHL, SHR, ADD, SUB, MUL, DIV, MOD,
+    OR, XOR, AND, EQ, NE, LT, GT, LE, GE,
+    SHL, SHR, ADD, SUB, MUL, DIV, MOD,
     OPEN, READ, CLOS, PRTF, MALC, MSET, MCMP, EXIT
 };
 
 int eval() {
     int op;
     while (1) {
-        op = *pc++; // get next operation code
+        op = *pc; // get the instruction to be executed in this cpu cycle
 
-        if (op == IMM) { // load immediate value to ax
-        	ax = *pc;
-            pc++;
+        pc++; // move the program counter pc
+              // to let it point to the operand of current instruction
+              // or next instruction
+
+        if (op == IMM) {
+        	ax = *pc; // load immediate value to ax
+
+            pc++; // move the pc pointer
         }  else if (op == PUSH) {
-            --sp; // move the stack pointer to insert an element into the stack
-        	*sp = ax;
+            --sp; // move the sp pointer to increase the size of the stack
+
+        	*sp = ax; // put the value of ax into the stack
         } else if (op == ADD) {
-        	ax = *sp + ax; // add the value in ax register with the value in stack top
-            sp++; // move the stack pointer to pop an element off the stack
+        	ax = *sp + ax; // add the value in ax with the value in stack top
+
+            sp++; // move the sp pointer to decrease the size of the stack
         } else if (op == EXIT) {
         	printf("exit(%d)\n", *sp); // print out the value in stack top
-        	return *sp; // on exit instruction, shut down the virtual machine
+
+        	return *sp; // on EXIT instruction, step out the while loop,
+                        // this means shutting down the virtual machine
         } else {
             printf("unknown instruction: %d\n", op);
-            return -1; // on unknown instruction, shut down the virtual machine
+
+            return -1; // on unknown instruction, also step out the while loop,
+                       // this means shutting down the virtual machine
         }
     }
 
     return 0;
 }
 
-int main(int argc, char *argv[]) {
+int main() {
 
 	poolsize = 256; // assume the size of text area and stack area is 256 bytes
-    // since one instruction need 8 bytes space, the text area contains max 32 instructions
+    // since one instruction need 4 or 8 bytes space,
+    // in this case, the text area contains max 64 instructions
 
-    // The malloc() function allocates size bytes of memory and returns a
+    // The malloc(size_t size) function allocates size bytes of memory and returns a
     // pointer to the allocated memory.
 
     // allocate memory for text area
@@ -61,11 +78,12 @@ int main(int argc, char *argv[]) {
     }
 
     memset(text, 0, poolsize); // initialize the text area to 0
+
     memset(stack, 0, poolsize); // initialize the stack area to 0
 
-    sp = (int *)((int *)stack + poolsize); // the stack moves from hight address towards low address
+    sp = (int *)((int *)stack + poolsize); // the stack grows from hight address towards low address
 
-    ax = 0; // initialize the value of ax register to 0
+    ax = 0; // initialize the value of ax to 0
 
     // put some testing instructions into text area
     int i = 0;
---
+++ Lesson_1/compiler1.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <fcntl.h> // for open()
+#include <unistd.h> // for read() and close()
+#include <stdlib.h> // for malloc()
+
+int token; // current token
+
+char *src; // pointer to source code string
+
+int poolsize; // the size of source code area
+
+void next() {
+    token = *src; // assign the char which the src pointer points to to variable token
+    src++; // move to next char
+}
+
+void program() {
+    next();                  // get next token
+    while (token > 0) {
+        printf("token is: %c\n", token);
+        next();
+    }
+}
+
+int main(int argc, char *argv[]) {
+
+  argv++;
+
+  poolsize = 256 * 1024; // assume the default size of source area is 256K
+
+  // open the source file and store the file descriptor to variable fd
+  int fd;
+  if ((fd = open(*argv, 0)) < 0) {
+    printf("could not open source file: %s\n", *argv);
+    return -1;
+  }
+
+  // allocate memory for source area
+  if (!(src = malloc(poolsize))) {
+    printf("could not malloc memory (%d bytes) for source area\n", poolsize);
+    return -1;
+  }
+
+  // read the source file to source area
+  // the last byte is reserved for EOF
+  int i;
+  if ((i = read(fd, src, poolsize-1)) <= 0) {
+    printf("could not read data from file: read() returned %d\n", i);
+    return -1;
+  }
+
+  close(fd); // close file
+
+  src[i] = -1; // add EOF character
+
+  program();
+}
